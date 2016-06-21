6c71e38b3f69da461942c1e243c8f16c92853906
Teddy Foley <tef77777@gmail.com>
2016-06-21 17:29:37 +0000

    Done.

---
+++ lib/holiday_greeting.rb
@@ -0,0 +1,7 @@
+def holiday_greeting(holiday = "Mother's Day", mother = "Mom", sender = "Your Favorite Child")
+  "Happy #{holiday}, #{mother}! From #{sender}"
+end
+
+
+## Call your method below - use puts to see it in the terminal!
+puts holiday_greeting
--- lib/mothers_day.rb
+++ lib/mothers_day.rb
@@ -1,6 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day(mother = "mom")
+  "Happy Mother's Day, #{mother}!"
+end
 
 
 ## Call your method below - use puts to see it in the terminal!
-
+puts mothers_day
